86b0760b894095ec037cf48d40a2bda3579daf44
peeradech12 <51998280+peeradech12@users.noreply.github.com>
2019-06-24 23:49:53 +0700

    add new file
    .exe file and .cpp file

---
+++ profile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	cout << "ID    : 62-060216-1210-5" << endl;
+	cout << "Name  : Peeradech Sadsum-ang" << endl;
+	cout << "Sex   : Male" << endl;
+	cout << "Age   : 18" << endl;
+	cout << "Hobby : Watches stars" << endl;
+	cout << "Thank for watching" << endl;
+	return (0);
+}
